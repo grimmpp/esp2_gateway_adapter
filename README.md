0ea853fc64103a5e17fcb3327d7a6fa3893103bf
Philipp Grimm <grimm.philipp@gmail.com>
2024-03-20 11:16:34 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# enocean-esp3-to-esp3-gateway-adapter
-enocean gateway adapter from ESP3 to ESP2 for eltako home assistant integration
+# EnOcean ESP3 to ESP3 Gateway Adapter
+
+EnOcean gateway adapter from ESP3 to ESP2 for [Eltako Home Assistant Integration](https://github.com/grimmpp/home-assistant-eltako).
+
+This component is mainly required to support USB300, USB400 and USB500 (Wireless EnOcean USB stick/transceiver) which is using ESP3 protocol unlike all other Eltako devices which are using ESP2.
---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+from distutils.core import setup
+
+
+
+base_dir = os.path.dirname(__file__)
+
+# with open(os.path.join(base_dir, 'README.md'), encoding="utf-8") as f:
+with open('README.md', encoding="utf-8") as f:
+    long_description = f.read()
+
+# with open(os.path.join(base_dir, 'LICENSE'), encoding="utf-8") as f:
+with open('LICENSE', encoding="utf-8") as f:
+    license = f.read()
+
+required = ['eltako14bus>=0.0.46', 'enocean>=0.60.1', 'pyserial', 'pyserial-asyncio', 'aiocoap']
+
+
+
+setup(
+    name='esp2_gateway_adapter',
+    version='0.1',
+    package_dir={"esp2_gateway_adapter":'src'},
+    package=[""],
+    include_package_data=True,
+    install_requires=required,
+    author="Philipp Grimm",
+    description="Protocol adapter from esp3 to esp2 for Home Assistant Eltako Integration",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license=license,
+    url="https://github.com/grimmpp/esp2_gateway_adapter",
+    python_requires='>=3.7',
+)
+
+#.\.venv\Scripts\python.exe -m build --sdistx1xxx
+#python setup.py bdist_wheel
